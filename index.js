caf264d97cc6013baf50f0f831edcc09393b1ff7
voracious-blog-1591 <zelayaromie@gmail.com>
2019-06-15 14:35:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var newArr= [];
+  for (var i = 0; i < musicians.length - 1; i ++) {
+    var str = musicians[i] + instruments[i]
+    newArr.push(str)
+  }
+  return newArr
+}
